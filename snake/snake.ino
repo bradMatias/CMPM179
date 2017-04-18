ebeecf55d82ac8238e215fe4d0caad0e97f9079f
bradMatias <bmatias@ucsc.edu>
2017-04-18 02:48:34 -0700

    added food

--- snake/snake.ino
+++ snake/snake.ino
@@ -17,21 +17,23 @@ NESpad nes = NESpad(6, 7, 5);
 byte state;
 
 //format for byte array
-//B??XXXYYY
-//highest order bits unused
-//0-2 used for Y Coordinate
-//3-5 used for X Coordinate
+//BXXXXYYYY
+//0-3 used for Y Coordinate
+//4-7 used for X Coordinate
 
+//coords start at 1 and goto 8 as a workaround for non-signed
 byte convertXY(int x, int y);
 
 size_t snakeLength = 3; //current length of the snake
 const size_t totalLength = 12;
-char snake[totalLength] = {convertXY(0,7),convertXY(1,7),convertXY(2,7)}; //starting on top left
+byte snake[totalLength] = {convertXY(1,8),convertXY(2,8),convertXY(3,8)}; //starting on top left
 char direction = 'r'; // r l u d // right left up down
 //padded 1 pixel top and bottom // array listed in rows
 byte gameOverImg[6] = {B01100110,B01100110,B00000000,B00000000,B01111110,B01000010};
 unsigned int timer = 0;
 bool dirChosen = false;
+byte foodPos;
+bool foodFlicker = true;
 
 void setup() {
   //The MAX72XX is in power-saving mode on startup, we have to do a wakeup call   
@@ -41,38 +43,49 @@ void setup() {
   //and clear the display
   lc.clearDisplay(0);
   Serial.begin(9600);
+  randomSeed(analogRead(0));
+  foodPos = convertXY(floor(random(8))+1,floor(random(8))+1);
 }
 
+
+//problem is you cant collect food when u pick it up mid turn
 void loop() {
   takeInput();
   timer++;
+  if(timer % 200 == 0){
+    lc.setLed(0,getX(foodPos)-1,getY(foodPos)-1,foodFlicker);
+    foodFlicker = !foodFlicker;
+    //Serial.println(getX(foodPos)-1);
+    //Serial.println(getY(foodPos)-1);
+  }
   if(timer==1000){
     moveSnake();
     lc.clearDisplay(0);
     for(int i=0;i<snakeLength;i++)
-      lc.setLed(0,getX(snake[i]),getY(snake[i]),true);
+      lc.setLed(0,getX(snake[i])-1,getY(snake[i])-1,true);
     timer = 0;
     dirChosen = false;
   }
 }
 
 byte convertXY(int x, int y){
-  return (((byte) x) << 3) | ((byte) y);
+  return (((byte) x) << 4) | ((byte) y);
 }
 
 int getX(byte snakeLink){
-  return (int) (B00111000 & snakeLink) >> 3;
+  return (int) (B11110000 & snakeLink) >> 4;
 }
 
 int getY(byte snakeLink){
-  return (int) (B00000111 & snakeLink);
+  return (int) (B00001111 & snakeLink);
 }
 
 void gameOver(){
+  foodPos = convertXY(floor(random(8))+1,floor(random(8))+1);
   snakeLength = 3;
   direction = 'r';
   for(int i=0;i<snakeLength; i++){
-    snake[i] = convertXY(i,7);
+    snake[i] = convertXY(i+1,8);
   }
   lc.clearDisplay(0);
   for(int i=0;i<6;i++)//the X and Y and row and columns are so backwards lol
@@ -86,32 +99,48 @@ void moveSnake(){
   }//moving each link up
   
   if(direction=='l'){      // left
-    if(getX(snake[snakeLength-2]) == 0){
+    snake[snakeLength-1] = convertXY(getX(snake[snakeLength-1]) - 1, getY(snake[snakeLength-1]));
+    if(getX(snake[snakeLength-1]) == 0){
       gameOver();
       return;
+    }else if(convertXY(getX(snake[snakeLength-1]) - 1, getY(snake[snakeLength-1])) == foodPos){
+      foodPos = convertXY(floor(random(8))+1,floor(random(8))+1);
+      snake[snakeLength] = convertXY(getX(snake[snakeLength-1]) - 1, getY(snake[snakeLength-1]));
+      snakeLength++;
     }
-    snake[snakeLength-1] = convertXY(getX(snake[snakeLength-1]) - 1, getY(snake[snakeLength-1]));
   }
   else if(direction=='r'){ // right
-    if(getX(snake[snakeLength-2]) == 7){
+    snake[snakeLength-1] = convertXY(getX(snake[snakeLength-1]) + 1, getY(snake[snakeLength-1]));
+    if(getX(snake[snakeLength-1]) == 9){
       gameOver();
       return;
+    }else if(convertXY(getX(snake[snakeLength-1]) + 1, getY(snake[snakeLength-1])) == foodPos){
+      foodPos = convertXY(floor(random(8))+1,floor(random(8))+1);
+      snake[snakeLength] = convertXY(getX(snake[snakeLength-1]) + 1, getY(snake[snakeLength-1]));
+      snakeLength++;
     }
-    snake[snakeLength-1] = convertXY(getX(snake[snakeLength-1]) + 1, getY(snake[snakeLength-1]));
   }
   else if(direction=='u'){ // up
-    if(getY(snake[snakeLength-2]) == 7){
+    snake[snakeLength-1] = convertXY(getX(snake[snakeLength-1]), getY(snake[snakeLength-1]) + 1);
+    if(getY(snake[snakeLength-1]) == 9){
       gameOver();
       return;
+    }else if(convertXY(getX(snake[snakeLength-1]), getY(snake[snakeLength-1]) + 1) == foodPos){
+      foodPos = convertXY(floor(random(8))+1,floor(random(8))+1);
+      snake[snakeLength] = convertXY(getX(snake[snakeLength-1]), getY(snake[snakeLength-1]) + 1);
+      snakeLength++;
     }
-    snake[snakeLength-1] = convertXY(getX(snake[snakeLength-1]), getY(snake[snakeLength-1]) + 1);
   }
   else{                    // down
-    if(getY(snake[snakeLength-2]) == 0){
+    snake[snakeLength-1] = convertXY(getX(snake[snakeLength-1]), getY(snake[snakeLength-1]) - 1);
+    if(getY(snake[snakeLength-1]) == 0){
       gameOver();
       return;
+    }else if(convertXY(getX(snake[snakeLength-1]), getY(snake[snakeLength-1]) - 1) == foodPos){
+      foodPos = convertXY(floor(random(8))+1,floor(random(8))+1);
+      snake[snakeLength] = convertXY(getX(snake[snakeLength-1]), getY(snake[snakeLength-1]) - 1);
+      snakeLength++;
     }
-    snake[snakeLength-1] = convertXY(getX(snake[snakeLength-1]), getY(snake[snakeLength-1]) - 1);
   }
 }
 
