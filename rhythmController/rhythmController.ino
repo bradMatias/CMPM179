26eeaef4759ab3ed0748f76504ce887663357257
Brad Matias <bmatias@ucsc.edu>
2017-04-25 16:41:02 -0700

    added Joystick library and small example

--- rhythmController/rhythmController.ino
+++ rhythmController/rhythmController.ino
@@ -1,11 +1,25 @@
+#include <Joystick.h>
+
+//creating joystick
+Joystick_ joy(JOYSTICK_DEFAULT_REPORT_ID, 
+  JOYSTICK_TYPE_MULTI_AXIS, 32, 0,
+  true, true, true, true, true, true,
+  true, true, true, true, true);
+
 //1=gnd 2=analogOut 3=5volt
 void setup() {
-
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  //range 0 - 1023
+  joy.begin(true);//send data immeadiately
 }
 
 void loop() {
-  Serial.println(analogRead(A5));
-  delay(50);
+  //Serial.println(analogRead(A5));
+  joy.setXAxis(analogRead(A5));
+  joy.setZAxis(analogRead(A5));
+  joy.setYAxis(512);
+  joy.setRxAxis(analogRead(A5));
+  delay(16);
 }
