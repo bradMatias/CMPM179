f4871d8b2e6831c24d71b8d3ef70110a6ffe8d55
bradMatias <bmatias@ucsc.edu>
2017-06-01 01:57:06 -0700

    game now has lasers

--- rhythmController/rhythmController.ino
+++ rhythmController/rhythmController.ino
@@ -1,5 +1,7 @@
 #include <Keyboard.h>
 
+#include "HID-Project.h"
+
 //1=gnd 2=analogOut 3=5volt
 
 byte b1Pin = 1;
@@ -17,10 +19,18 @@ bool f = false;
 bool j = false;
 bool k = false;
 
+int offTimer = 0;
+
+int mouseX = 0;
+int mouseY = 0;
+
+bool active = true;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   Keyboard.begin();
+  AbsoluteMouse.begin();
   pinMode(b1Pin, INPUT_PULLUP);
   pinMode(b2Pin, INPUT_PULLUP);
   pinMode(b3Pin, INPUT_PULLUP);
@@ -32,55 +42,77 @@ void setup() {
 }
 
 void loop() {
-  //button 1 input 'd'
-  if(digitalRead(b1Pin) == LOW){
-    Keyboard.press('d');
-    if(!d){
-      digitalWrite(led1Pin,HIGH);
+  mouseX = (analogRead(A1) * 32);//value is 16 bit signed, so [-32768, 32767]
+  mouseY = analogRead(A2) * 32;//the analog is [0,1024] so * by 32 to put 16bit range
+  
+  if(active){
+    AbsoluteMouse.moveTo(mouseX,mouseY); 
+  
+    //button 1 input 'd'
+    if(digitalRead(b1Pin) == LOW){
+      Keyboard.press('d');
+      if(!d){
+        digitalWrite(led1Pin,HIGH);
+      }
+      d = true;
+    }else if(d){
+      Keyboard.release('d');
+      digitalWrite(led1Pin,LOW);
+      d = false;
     }
-    d = true;
-  }else if(d){
-    Keyboard.release('d');
-    digitalWrite(led1Pin,LOW);
-    d = false;
+  
+    //button 2 input 'f'
+    if(digitalRead(b2Pin) == LOW){
+      Keyboard.press('f');
+      if(!f){
+        digitalWrite(led2Pin,HIGH);
+      }
+      f = true;
+    }else if(f){
+      Keyboard.release('f');
+      digitalWrite(led2Pin,LOW);
+      f = false;
+    }  
+  
+    //button 3 input 'j'
+    if(digitalRead(b3Pin) == LOW){
+      Keyboard.press('j');
+      if(!j){
+        digitalWrite(led3Pin,HIGH);
+      }
+      j = true;
+    }else if(j){
+      Keyboard.release('j');
+      digitalWrite(led3Pin,LOW);
+      j = false;
+    }  
+  
+    //button 4 input 'k'
+    if(digitalRead(b4Pin) == LOW){
+      Keyboard.press('k');
+      if(!k){
+        digitalWrite(led4Pin,HIGH);
+      }
+      k = true;
+    }else if(k){
+      Keyboard.release('k');
+      digitalWrite(led4Pin,LOW);
+      k = false;
+    }
   }
 
-  //button 2 input 'f'
-  if(digitalRead(b2Pin) == LOW){
-    Keyboard.press('f');
-    if(!f){
-      digitalWrite(led2Pin,HIGH);
+  if(d && f && j && k){
+    offTimer++;
+    if(offTimer == 500){
+      active = false;
+      Keyboard.releaseAll();
+      Keyboard.end();
+      digitalWrite(led1Pin,LOW);
+      digitalWrite(led2Pin,LOW);
+      digitalWrite(led3Pin,LOW);
+      digitalWrite(led4Pin,LOW);
     }
-    f = true;
-  }else if(f){
-    Keyboard.release('f');
-    digitalWrite(led2Pin,LOW);
-    f = false;
-  }  
-
-  //button 3 input 'j'
-  if(digitalRead(b3Pin) == LOW){
-    Keyboard.press('j');
-    if(!j){
-      digitalWrite(led3Pin,HIGH);
-    }
-    j = true;
-  }else if(j){
-    Keyboard.release('j');
-    digitalWrite(led3Pin,LOW);
-    j = false;
-  }  
-
-  //button 4 input 'k'
-  if(digitalRead(b4Pin) == LOW){
-    Keyboard.press('k');
-    if(!k){
-      digitalWrite(led4Pin,HIGH);
-    }
-    k = true;
-  }else if(k){
-    Keyboard.release('k');
-    digitalWrite(led4Pin,LOW);
-    k = false;
+  }else{
+    offTimer = 0;
   }
 }
